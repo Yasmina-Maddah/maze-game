71aa7d4f463c6aa3487fe940f476ad08a9af7038
Yasmina Maddah <maddahyasmina@gmail.com>
2024-10-28 21:47:23 +0200

    Initial commit

---
+++ scripts/myTools.js
@@ -0,0 +1,5 @@
+var status = document.getElementById("status");
+var start = document.getElementById("start");
+var end = document.getElementById("end");
+var boundaries = document.querySelector(".boundary");
+
