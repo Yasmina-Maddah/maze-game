8c4d271a3d81fdd6dc0af70a816188cfb6f148ed
Yasmina Maddah <maddahyasmina@gmail.com>
2024-10-28 22:02:11 +0200

    Added  JavaScript logic to start and end the game based on cursor movement

--- scripts/myTools.js
+++ scripts/myTools.js
@@ -3,3 +3,18 @@ var start = document.getElementById("start");
 var end = document.getElementById("end");
 var boundaries = document.querySelector(".boundary");
 
+var gameStarted = "false";
+var gameLost = "false";
+
+start.addEventListener("mouseover", () => {
+    gameStarted = true;
+    gameLost = false;
+    status.textContent = "Game started! Guide the mouse to 'E'!";
+    resetBoundaries();
+})
+
+function resetBoundaries() {
+    boundaries.forEach(boundary => {
+      boundary.classList.remove("highlighted");
+    });
+  }
